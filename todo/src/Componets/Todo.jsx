cd22b099d03d326aa97f1c27987f5f5aef7d0f3b
ap-lodhi <anilpatel.bcs@gmail.com>
2023-07-15 16:03:56 +0530

    shopping-cart

--- todo/src/Componets/Todo.jsx
+++ todo/src/Componets/Todo.jsx
@@ -1,38 +1,52 @@
 import React, { useState } from "react";
-import './todo.css'
+import "./todo.css";
 
 function Todo() {
-  const [tasks, setTasks] = useState([]);
+  //use state for storing the task.
+  //task with an id, content, and completed flag
+  const [data, setData] = useState([]);
+
+  //   console.log(data);
+
+  // this is also similar for taking to input ans set
   const [newTask, setNewTask] = useState("");
+
+  //The handleInputChange function is triggered when the user types in the input field, 
+  //updating the newTask state. The handleAddTask function is called when the 
+  //user clicks the "Add Task" button, adding the new task to the tasks state array.
 
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
   };
-
+// this funtion will add the task 
   const handleAddTask = () => {
     if (newTask.trim() !== "") {
-      setTasks([
-        ...tasks,
+      setData([
+        ...data,
         { id: Date.now(), content: newTask, completed: false },
       ]);
       setNewTask("");
     }
   };
 
+  // this the for checking if task is complete or not 
+
   const handleCompleteTask = (taskId) => {
-    setTasks(
-      tasks.map((task) =>
+    setData(
+      data.map((task) =>
         task.id === taskId ? { ...task, completed: !task.completed } : task
       )
     );
   };
 
+
+  // this function for removing if we want we can remove the task  
   const handleRemoveTask = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
+    setData(data.filter((task) => task.id !== taskId));
   };
 
-  const totalTasks = tasks.length;
-  const completedTasks = tasks.filter((task) => task.completed).length;
+  const totalTasks = data.length;
+  const completedTasks = data.filter((task) => task.completed).length;
 
   return (
     <div className="main_div">
@@ -50,7 +64,10 @@ function Todo() {
         <h2>Total tasks: {totalTasks}</h2>
         <h2>Completed tasks: {completedTasks}</h2>
         <ul>
-          {tasks.map((task) => (
+
+
+            {/* here is mapped all the data which will reflect on ui  */}
+          {data.map((task) => (
             <li key={task.id}>
               <input
                 type="checkbox"
@@ -62,7 +79,7 @@ function Todo() {
                   textDecoration: task.completed ? "line-through" : "none",
                 }}
               >
-               {task.content}
+                {task.content}
               </span>
               <button
                 onClick={() => handleRemoveTask(task.id)}
